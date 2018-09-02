bcf0996a23c8dc7ef461b1a36bbf9cf5e0a660a7
Nickghost inc <you@example.com>
2018-09-02 20:42:26 +0300

    views.py updated

--- app/main/views.py
+++ app/main/views.py
@@ -1,25 +1,26 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_source_names, get_articles
+from ..request import get_sources,get_article
+
 
 @main.route('/')
 def index():
-    '''
-    Functiont that returns the index page and its data
-    '''
-    title = 'Trending News Base'
+    """
+    Function that returns the index page and all its data
+    """
+    entertainment_news = get_sources('entertainment')
+    sport_news = get_sources('sports')
+    tech_news = get_sources('technology')
+    title = 'News Highlights by Ephantus'
+    return render_template('index.html', title=title, sports=sport_news, technology=tech_news, entertainment=entertainment_news)
 
-    sources_and_name = get_source_names('sources')
-    return render_template('index.html', title= title ,sources_and_name = sources_and_name)
 
-
-@main.route('/news_page/<source_id>')
-def news_page(source_id):
-    '''
-    Function that returns the highlights page and its data
-    '''
-    title = 'Highlights'
-    # this parts get different articles from a specified source
-
-    source_and_articles = get_articles(source_id)
-    return render_template('news_page.html', title = title, source_and_articles= source_and_articles )  
+@main.route('/source/<id>')
+def articles(id):
+    """
+    View articles
+    """
+    article = get_article(id)
+    print(article)
+    title = f'Ephy News ~ {id}'
+    return render_template('news.html', title=title, article=article)
