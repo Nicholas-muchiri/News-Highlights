ba2df7aadb70e1f040d4ef48c510603ed7c75c60
Nickghost inc <you@example.com>
2018-09-02 20:55:57 +0300

    __init__.py file code changed

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,25 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap       
+from flask_bootstrap import Bootstrap
 from config import config_options
 
 bootstrap = Bootstrap()
 
+
 def create_app(config_name):
+    app = Flask(__name__)
 
-    app = Flask (__name__)
-
-    #this part is creating app configuration
-
+    # create the app configurations
     app.config.from_object(config_options[config_name])
 
-    #initializing flask extensions 
-
+    # Initializing flask extensions
     bootstrap.init_app(app)
 
-    #this part helps to register the blueprint
-    
+    # Registering the blueprint
     from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)    
+    app.register_blueprint(main_blueprint)
 
-    # setting the config 
+    # setting config
+    from .request import configure_request
+    configure_request(app)
 
-    from .requests import config_func
-    config_func(app)
-    
     return app
